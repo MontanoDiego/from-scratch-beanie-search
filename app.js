e2cb8b5d3860ee8b409d2738e1a8d25039f03fec
MontanoDiego <diego.e2.montano@gmail.com>
2022-12-06 15:47:10 -0800

    beanie babies now render properly to page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Imports */
 
 import { getBeanieBabies, getAnimalTypes } from './fetch-utils.js';
-import { renderAnimalOptions } from './render-utils.js';
+import { renderAnimalOptions, renderBaby } from './render-utils.js';
 /* Get DOM Elements */
 const beanieList = document.getElementById('babies-container');
 const form = document.getElementById('search-form');
@@ -13,17 +13,33 @@ let animalTypes = [];
 
 /* Events */
 self.addEventListener('load', async () => {
+    findBeanies();
     const response = await getAnimalTypes();
     animalTypes = response;
     await displayAnimalOptions();
+    displayBeanies();
 });
 
+async function findBeanies(animalTypes) {
+    const response = await getBeanieBabies(animalTypes);
+    beanies = response;
+    displayBeanies();
+}
 
 /* Display Functions */
 function displayAnimalOptions() {
     for (let animal of animalTypes) {
         const option = renderAnimalOptions(animal);
         animalSelect.append(option);
+    }
+}
+
+function displayBeanies() {
+    beanieList.innerHTML = '';
+
+    for (let beanie of beanies) {
+        const beanieEl = renderBaby(beanie);
+        beanieList.append(beanieEl);
     }
 }
 
--- render-utils.js
+++ render-utils.js
@@ -1,21 +1,27 @@
-// export function renderBaby(baby) {
-//     const li = document.createElement('li');
-//     const h2 = document.createElement('h2');
-//     const p = document.createElement('p');
-// 
-// 
-//     const img = document.createElement('img');
-//     img.src = baby.image;
-//     img.alt = baby.title;
-// 
-//     h2.textContent = baby.title;
-// 
-//     p.textContent = baby.releaseDate;
-// 
-//     li.append(h2, img, p);
-// 
-//     return li;
-// }
+export function renderBaby(baby) {
+    const li = document.createElement('li');
+    li.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = baby.image;
+    img.alt = baby.title;
+
+
+    const h2 = document.createElement('h2');
+    h2.textContent = baby.title;
+
+    const p = document.createElement('p');
+    if (baby.releaseYear === null) {
+        p.textContent = 'Released: unknown';
+    } else {
+        p.textContent = 'Released: ' + baby.releaseYear;
+    }
+    p.classList.add('attributes');
+    
+    li.append(h2, img, p);
+
+    return li;
+}
 
 export function renderAnimalOptions(animalType) {
     const option = document.createElement('option');
